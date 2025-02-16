c04eb69e4fef35ffd095f4f5758e1e05bca62600
MasherNJIT <157054958+MasherNJIT@users.noreply.github.com>
2025-02-16 11:12:05 -0500

    Add files via upload

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -11,16 +11,39 @@ if (isset($argv[1]))
 }
 else
 {
-  $msg = "test message";
+  $msg = "login request sent";
 }
 
 $request = array();
+$registration = array(); 
+/*
 $request['type'] = "Login";
 $request['username'] = "steve";
 $request['password'] = "password";
+^Use for testing purposes only without login page^
+*/ 
+//Sending Login
+$request['type'] = "login";
+$request['username'] = $_POST['username'];
+$request['password'] = $_POST['password'];
+
+
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
+
+
+
+if($response['returnCode'] == 1) //This picks up return code 
+//if the front-end recieves a message from the MQ with a return code of 1, it means the login is successful 
+{
+  header("Location: index.php"); 
+  //echo "Heres the username" .$request['username'].   "and heres the password"  .$request['password']; //NOTE: this is just testing to make sure that hte username and password went over
+}
+else if ($response['returnCode'] == 0) //returns user back to login page if login is a failure 
+{
+  header("Location: index.html");
+}
 
 echo "client received response: ".PHP_EOL;
 print_r($response);
