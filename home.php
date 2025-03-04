ae4b250e5c4b73324d4b6e0f16860dbf875e2e6d
heatherrabanes <heatherrabanes@gmail.com>
2025-03-04 01:12:07 -0500

    updating navbar

--- home.php
+++ home.php
@@ -4,7 +4,9 @@ include('partials/nav.php');
 
 <link rel="stylesheet" href="main.css"; ?>
 
+<div>
 <h1>Home</h1>
+</div>
 
 
 
--- partials/nav.php
+++ partials/nav.php
@@ -4,6 +4,12 @@
 </head>
     <nav>
         <ul>
+        <li><a href="../home.php">Home</a></li>
+        <li><a href=>Profile</a></li>
+        <li><a href=>MLS</a></li>
+        <li><a href=>Players</a></li>
+        <li><a href=>Rules</a></li>
+        <li><a href=>Leagues</a></li>
         <li><a href="../logout.php">Logout</a></li>
         </ul>
     </nav>
