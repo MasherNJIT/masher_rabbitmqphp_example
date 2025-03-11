904fd93a923ae331f4a6480a9bf13e56fc4206d6
Mystikal17 <fss22@njit.edu>
2025-03-11 13:05:19 +0000

    Fixed for pathing

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -3,7 +3,7 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('teams.php');
+require_once('bpl/Manchester_City.php');
 
 $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
