801e06db3aafb105131c77f6d61406c0aebd6fab
Mystikal17 <fss22@njit.edu>
2025-03-11 12:50:49 +0000

    Tested api for both teams

---
+++ bpl/Manchester_City.php
@@ -0,0 +1,38 @@
+#!/usr/bin/php
+<?php
+$apiKey = "485649";
+$teamName = "Manchester City";
+$apiUrl = "https://www.thesportsdb.com/api/v1/json/{$apiKey}/searchplayers.php?t=" . urlencode($teamName);
+
+$results = file_get_contents($apiUrl);
+
+$arrayCode = json_decode($results, true);
+
+if ($arrayCode === null) {
+    die("Error: Unable to retrieve data. Check API key or endpoint.");
+}
+
+$filteredPlayers = [];
+
+if (!empty($arrayCode['player'])) {
+    foreach ($arrayCode['player'] as $player) {
+        if (isset($player['strPosition']) &&
+            strpos($player['strTeam'], "Women") === false && 
+            strpos($player['strTeam'], "U21") === false &&
+            strpos($player['strTeam'], "U23") === false &&
+            $player['strPosition'] !== "Manager") {
+            
+            $filteredPlayers[] = [
+                'name' => $player['strPlayer'] ?? "",
+                'position' => $player['strPosition'] ?? "",
+                'idPlayer' => $player['idPlayer'] ?? "",
+                'idTeam' => $player['idTeam'] ?? ""
+            ];
+        }
+    }
+}
+
+echo "<pre>";
+print_r($filteredPlayers);
+echo "</pre>";
+?>
