c7bfe7791326315dce0095d0e8364e3b8c5e12ec
Mystikal17 <fss22@njit.edu>
2025-03-11 02:04:04 +0000

    working teams api

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -3,28 +3,42 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
+require_once('teams.php');
 
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-if (isset($argv[1]))
-{
-  $msg = $argv[1];
-}
-else
-{
-  $msg = "test message";
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+
+$apiKey = "485649";
+$apiUrl = "https://www.thesportsdb.com/api/v1/json/{$apiKey}/search_all_teams.php?l=English%20Premier%20League";
+
+$results = file_get_contents($apiUrl);
+$teamsData = json_decode($results, true);
+
+if ($teamsData === null || empty($teamsData['teams'])) {
+    die("Error: Unable to retrieve team data. Check API key or endpoint.");
 }
 
-$request = array();
-$request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
-$request['message'] = $msg;
+$allTeams = [];
+
+foreach ($teamsData['teams'] as $team) {
+    $allTeams[] = [
+        'team_name'   => $team['strTeam'] ?? "",
+        'team_id_api' => $team['idTeam'] ?? "",
+        'stadium'     => $team['strStadium'] ?? "",
+        'league'      => $team['strLeague'] ?? "",
+    ];
+}
+
+
+$request = [
+    'type'  => "APIteams",
+    'teams' => $allTeams
+];
+
 $response = $client->send_request($request);
-//$response = $client->publish($request);
 
-echo "client received response: ".PHP_EOL;
+echo "Sent all team data in one request." . PHP_EOL;
 print_r($response);
-echo "\n\n";
 
-echo $argv[0]." END".PHP_EOL;
+echo $argv[0] . " END" . PHP_EOL;
+?>
 
