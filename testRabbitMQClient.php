98f93894113ce5021d39fd7691bdb5d7d67a0c33
Mystikal17 <fss22@njit.edu>
2025-03-11 16:19:04 +0000

    Has all players from BPL

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -3,7 +3,28 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
+require_once('bpl/Arsenal.php');
+require_once('bpl/Aston_Villa.php');
+require_once('bpl/Bournemouth.php');
+require_once('bpl/Brentford.php');
+require_once('bpl/Brighton.php');
+require_once('bpl/Chelsea.php');
+require_once('bpl/Crystal_Palace.php');
+require_once('bpl/Everton.php');
+require_once('bpl/Fulham.php');
+require_once('bpl/Ipswich_Town.php');
+require_once('bpl/Leicester_City.php');
+require_once('bpl/Liverpool.php');
 require_once('bpl/Manchester_City.php');
+require_once('bpl/Manchester_United.php');
+require_once('bpl/Newcastle.php');
+require_once('bpl/Nottingham.php');
+require_once('bpl/Southampton.php');
+require_once('bpl/Tottenham.php');
+require_once('bpl/West_Ham.php');
+require_once('bpl/Wolves.php');
+
+
 
 $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
