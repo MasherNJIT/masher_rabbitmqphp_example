ea5f6a2b5ad7e069dc41b90a48a2971a6166f0a7
Evan <117389682+elp999@users.noreply.github.com>
2025-03-15 18:33:26 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,55 +1,50 @@
 <?php
-//CODE REFERENCED FROM: Mike Gabriel Ayson
+// CODE REFERENCED FROM: Mike Gabriel Ayson
+session_start();
+
 echo "logging in...";
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-if (isset($argv[1]))
-{
-  $msg = $argv[1];
-}
-else
-{
-  $msg = "login request sent";
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+
+if (isset($argv[1])) {
+    $msg = $argv[1];
+} else {
+    $msg = "login request sent";
 }
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 $d = time();
 
-if ($loggedin == true)
-{
-exit();
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+    header("Location: home.php");
+    exit();
 }
 
-$request = array(); 
-
-//Sending Login
+$request = array();
 $request['type'] = "login";
 $request['username'] = $username;
 $request['password'] = $password;
 $request['session'] = $d;
 $request['message'] = $msg;
+
 $response = $client->send_request($request);
 
-if($response['returnCode'] == 1) //This picks up return code 
-//if the front-end recieves a message from the MQ with a return code of 1, it means the login is successful 
-{
-
-  header("Location: home.php");
+if ($response['returnCode'] == 1) {
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    $_SESSION['user_id'] = $response['user_id'];
   
+    header("Location: home.php");
+    exit();
+} else if ($response['returnCode'] == 0) {
+    header("Location: index.php");
+    exit();
 }
 
-else if ($response['returnCode'] == 0) //returns user back to login page if login is a failure
-{
-  echo $response;
-  header("Location: index.php");
-  exit();
-}
-
-$loggedin = true;
 echo "client received response: ".PHP_EOL;
 print_r($response);
 echo "\n\n";
@@ -57,5 +52,3 @@ echo "\n\n";
 echo $argv[0]." END".PHP_EOL;
 ?>
 
-echo $argv[0]." END".PHP_EOL;
-?>
