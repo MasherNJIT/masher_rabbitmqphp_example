4aabe71ef1c8ba76be5e33a96ebc3577d455dd33
Evan <117389682+elp999@users.noreply.github.com>
2025-03-15 18:39:38 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,9 +1,14 @@
 <?php
 include('partials/nav.php');
-require_once('login.php');
+session_start();
 
-echo $username;
-echo $d;
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+    echo "Welcome to your profile, " . $_SESSION['username'];
+    echo "Your user ID is: " . $_SESSION['user_id'];
+} else {
+    header("Location: index.php");
+    exit();
+}
 ?>
 
 <link rel="stylesheet" href="main.css"; ?>
