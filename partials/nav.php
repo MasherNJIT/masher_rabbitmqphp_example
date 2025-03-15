2adb1b21c859281ce29a2ed3f7c5f5dc00a4f0b9
heatherrabanes <heatherrabanes@gmail.com>
2025-03-15 18:18:01 -0400

    navbar work

--- partials/nav.php
+++ partials/nav.php
@@ -8,8 +8,15 @@
     <script src="bootstrap-5.3.3-dist/js/bootstrap.js"></script>
 </head>
     <nav class="navbar-expand-md">
-        <ul>
-        <li><a href="../home.php">Home</a></li>
+    <button class="navbar-toggler" type="button"
+        data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="../home.php">Home</a>
+        </li>
         <li><a href="../epl.php">EPL</a></li>
         <li><a href="../players.php">Players</a></li>
         <li><a href="../rules.php">Rules</a></li>
