40a1462156658a34018fcdce3f3291401b45a96c
heatherrabanes <heatherrabanes@gmail.com>
2025-03-17 20:45:55 -0400

    creating layout for create leagues screen

--- createl.php
+++ createl.php
@@ -1,1 +1,83 @@
+<?php
 //This is for creating a league
+?>
+
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="main.css"; ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EPL Fantasy Soccer</title>
+    <link href="bootstrap-5.3.3-dist/css/bootstrap.css" rel="stylesheet">
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.js"></script>
+</head>
+
+<div>
+<h1>Create your league!</h1>
+</div>
+
+<div>
+<div class="container text-center league-creator">
+    <div class="row">
+        <div class="col player-selector">
+            GK
+        </div>
+        </div>
+    <div class="row"></div>
+    <div class="row">
+        <div class="col player-selector">
+        LB
+        </div>
+        <div class="col player-selector">
+        CB
+        </div>
+        <div class="col player-selector">
+        CB
+        </div>
+        <div class="col player-selector">
+        LB
+        </div>
+    </div>
+    <div class="row"></div>
+    <div class="row">
+        <div class="col player-selector">
+        LM
+        </div>
+        <div class="col player-selector">
+        CM
+        </div>
+        <div class="col player-selector">
+        CM
+        </div>
+        <div class="col player-selector">
+        RM
+        </div>
+    </div>
+    <div class="row"></div>
+    <div class="row">
+        <div class="col player-selector">
+        ST
+        </div>
+        <div class="col player-selector">
+        ST
+        </div>
+    </div>
+    <div class="row"></div>
+    <div class="row">
+        <div class="col player-selector">
+        SUB
+        </div>
+        <div class="col player-selector">
+        SUB
+        </div>
+        <div class="col player-selector">
+        SUB
+        </div>
+        <div class="col player-selector">
+        SUB
+        </div>
+</div>
+</div>
+
+</html>
+
